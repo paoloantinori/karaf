844c7d402fc2161359edf2ff989001119e8440cb
Guillaume Nodet <gnodet@gmail.com>
2012-11-29 09:36:27 +0100

    Make sure the console thread for the non blocking input stream is correctly shut down

--- shell/console/src/main/java/org/apache/karaf/shell/console/jline/Console.java
+++ shell/console/src/main/java/org/apache/karaf/shell/console/jline/Console.java
@@ -146,6 +146,8 @@ public class Console implements Runnable
         running = false;
         CommandSessionHolder.unset();
         pipe.interrupt();
+        thread.interrupt();
+        reader.shutdown();
         if (closedByUser && closeCallback != null) {
             closeCallback.run();
         }
