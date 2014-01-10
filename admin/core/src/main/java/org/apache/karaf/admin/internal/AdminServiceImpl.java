5f6c7dccb8fd0bd070a22f9e1ca626e368f3b672
Jonathan Anstey <janstey@gmail.com>
2014-01-10 10:46:38 -0330

    KARAF-2678 - txlog files locked during admin clone on windows

--- admin/core/src/main/java/org/apache/karaf/admin/internal/AdminServiceImpl.java
+++ admin/core/src/main/java/org/apache/karaf/admin/internal/AdminServiceImpl.java
@@ -1006,6 +1006,10 @@ public class AdminServiceImpl implements AdminService {
             // ignore lock file
             return;
         }
+        if (source.getName().matches("transaction_\\d+\\.log")) {
+            // ignore active txlog files
+            return;
+        }
         if (source.isDirectory()) {
             if (!destination.exists()) {
                 destination.mkdirs();
