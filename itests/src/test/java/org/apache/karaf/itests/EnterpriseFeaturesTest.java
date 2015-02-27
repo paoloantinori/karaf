d9cdfe3b5598ebde74e4e7217535970e6a4537af
Jonathan Anstey <janstey@gmail.com>
2015-02-27 14:45:03 -0330

    Remove tests for older hibernate features

--- itests/src/test/java/org/apache/karaf/itests/EnterpriseFeaturesTest.java
+++ itests/src/test/java/org/apache/karaf/itests/EnterpriseFeaturesTest.java
@@ -57,21 +57,12 @@ public class EnterpriseFeaturesTest extends KarafTestSupport {
     @Test
     public void testHibernateFeatures() throws Exception {
         System.out.println("");
-        System.out.println("===== TESTING HIBERNATE " + System.getProperty("hibernate3.version") + " FEATURE =====");
-        installAndAssertFeature("hibernate", System.getProperty("hibernate3.version"));
-        System.out.println("");
-        System.out.println("===== TESTING HIBERNATE " + System.getProperty("hibernate42.version") + " FEATURE =====");
-        installAndAssertFeature("hibernate", System.getProperty("hibernate42.version"));
-        System.out.println("");
         System.out.println("===== TESTING HIBERNATE " + System.getProperty("hibernate43.version") + " FEATURE =====");
         installAndAssertFeature("hibernate", System.getProperty("hibernate43.version"));
     }
 
     @Test
     public void testHibernateEnversFeatures() throws Exception {
-        System.out.println("");
-        System.out.println("===== TESTING HIBERNATE-ENVERS " + System.getProperty("hibernate42.version") + " FEATURE =====");
-        installAndAssertFeature("hibernate-envers", System.getProperty("hibernate42.version"));
         System.out.println("");
         System.out.println("===== TESTING HIBERNATE-ENVERS " + System.getProperty("hibernate43.version") + " FEATURE =====");
         installAndAssertFeature("hibernate-envers", System.getProperty("hibernate43.version"));
