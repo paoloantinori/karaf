e682dadaff6088ab036406a2aca51f10dce7ea19
Freeman Fang <freeman.fang@gmail.com>
2016-06-14 16:38:58 +0800

    [ENTESB-5683]get Exception when enter "tab" from karaf console

--- shell/console/src/main/java/org/apache/karaf/shell/console/completer/CommandsCompleter.java
+++ shell/console/src/main/java/org/apache/karaf/shell/console/completer/CommandsCompleter.java
@@ -151,6 +151,11 @@ public class CommandsCompleter implements Completer {
                 referenceField.setAccessible(true);
                 BundleContext context = (BundleContext) contextField.get(function);
                 ServiceReference reference = (ServiceReference) referenceField.get(function);
+                String[] ocs = (String[])reference.getProperty("objectClass");
+                if (context == null || reference == null || ocs == null
+                    || ocs[0].equals("org.apache.felix.scr.impl.ScrGogoCommand")) {
+                    return null;
+                }
                 Object target = context != null ? context.getService(reference) : null;
                 try {
                     if (target instanceof Function) {
