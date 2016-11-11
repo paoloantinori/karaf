106635c4ddd50d02b0de1c24489a1d5d669c037b
Paolo Antinori <pantinor@redhat.com>
2016-11-11 14:02:20 +0100

    ENTESB-6226 - Audit logout operations
    (cherry picked from commit 7cd09e98ef756e79470f58f56ee9a31153f7bc2c)

--- shell/commands/src/main/java/org/apache/karaf/shell/commands/LogoutAction.java
+++ shell/commands/src/main/java/org/apache/karaf/shell/commands/LogoutAction.java
@@ -17,15 +17,82 @@
 package org.apache.karaf.shell.commands;
 
 import org.apache.felix.gogo.commands.Command;
+import org.apache.karaf.jaas.boot.principal.UserPrincipal;
 import org.apache.karaf.shell.console.AbstractAction;
+import org.apache.karaf.shell.console.BundleContextAware;
 import org.apache.karaf.shell.console.CloseShellException;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+
+import javax.security.auth.Subject;
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.NameCallback;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import javax.security.auth.login.LoginContext;
+import java.io.IOException;
+import java.security.AccessControlContext;
+import java.security.AccessController;
+import java.util.Dictionary;
+import java.util.Set;
 
 @Command(scope = "shell", name = "logout", description = "Disconnects shell from current session.")
-public class LogoutAction extends AbstractAction {
+public class LogoutAction extends AbstractAction implements BundleContextAware{
+
+    protected BundleContext bundleContext;
 
     protected Object doExecute() throws Exception {
         log.info("Disconnecting from current session...");
+        ConfigurationAdmin admin = getConfigurationAdmin();
+
+        Configuration configuration = admin.getConfiguration("org.apache.karaf.shell");
+        Dictionary<String, Object> properties = configuration.getProperties();
+        String realm=(String) properties.get("sshRealm");
+
+        AccessControlContext context = AccessController.getContext();
+        Subject subject = Subject.getSubject(context);
+
+        Set<UserPrincipal> principals = subject.getPrincipals(UserPrincipal.class);
+        final String username = principals.iterator().next().getName();
+
+        LoginContext loginContext = new LoginContext(realm, subject, new CallbackHandler() {
+            public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+                for (Callback callback : callbacks) {
+                    if (callback instanceof NameCallback) {
+                        ((NameCallback) callback).setName(username);
+                    } else {
+                        throw new UnsupportedCallbackException(callback);
+                    }
+                }
+            }
+        });
+        loginContext.logout();
+
         throw new CloseShellException();
     }
 
+    protected ConfigurationAdmin getConfigurationAdmin() {
+        ServiceReference ref = getBundleContext().getServiceReference(ConfigurationAdmin.class.getName());
+        if (ref == null) {
+            return null;
+        }
+            ConfigurationAdmin admin = (ConfigurationAdmin) getBundleContext().getService(ref);
+            if (admin == null) {
+                return null;
+            } else {
+                return admin;
+            }
+        }
+
+
+
+    public BundleContext getBundleContext() {
+        return bundleContext;
+    }
+
+    public void setBundleContext(BundleContext bundleContext) {
+        this.bundleContext = bundleContext;
+    }
 }
